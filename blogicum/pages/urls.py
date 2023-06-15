12843aaa93ff23f8904a250aa345c13643f574fe
user-name <zinovev.evgenii@mail.ru>
2023-06-15 13:59:00 +0300

    2error

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,10 +1,26 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import include, path, reverse_lazy
+from django.views.generic.edit import CreateView
 
-from . import views
+handler404 = "pages.views.page_not_found"
+handler500 = "pages.views.server_error"
 
-app_name = "pages"
 
 urlpatterns = [
-    path("about/", views.AboutPage.as_view(), name="about"),
-    path("rules/", views.RulesPage.as_view(), name="rules"),
-]
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls", namespace="blog")),
+    path("pages/", include("pages.urls", namespace="pages")),
+    path("auth/", include("django.contrib.auth.urls")),
+    path(
+        "auth/registration/",
+        CreateView.as_view(
+            template_name="registration/registration_form.html",
+            form_class=UserCreationForm,
+            success_url=reverse_lazy("blog:index"),
+        ),
+        name="registration",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
