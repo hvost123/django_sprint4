6c021229f861e9af7b028715d08366b9b7175723
Evgeniy <zinovev.evgenii@mail.ru>
2023-06-29 16:39:40 +0300

    round1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -168,12 +168,13 @@ def add_comment(request, pk):
 def edit_comment(request, comment_id, post_id):
     '''Изменение комментария'''
     instance = get_object_or_404(Comment, id=comment_id, post_id=post_id)
-    if instance.author != request.user: 
+    if instance.author != request.user:
         form = CommentForm(request.POST or None, instance=instance)
         context = {
             'form': form,
             'comment': instance
     }
+        
     if form.is_valid():
         form.save()
         return redirect('blog:post_detail', pk=post_id)
@@ -184,7 +185,7 @@ def edit_comment(request, comment_id, post_id):
 def delete_comment(request, comment_id, post_id):
     '''Удаление комментария'''
     instance = get_object_or_404(Comment, id=comment_id, post_id=post_id)
-    if instance.author != request.user: 
+    if instance.author != request.user:
         context = {'comment': instance}
     if request.method == 'POST':
         instance.delete()
